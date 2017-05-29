895e3979f29b340e36ff3a4665a925f642a277c8
Al <albarrentine@gmail.com>
2017-05-29 05:54:51 -0400

    [input] GeoJSON line/FeatureCollection parsing

---
+++ lib/lieu/input.py
@@ -0,0 +1,32 @@
+import ujson as json
+
+
+class GeoJSONParser(object):
+    def __init__(self, filename):
+        self.i = 0
+        self.data = json.load(open(filename))
+        self.features = self.data.get('features', [])
+        self.num_features = len(self.features)
+
+    def __iter__(self):
+        self.i = 0
+        return self
+
+    def next_feature(self):
+        if self.i < self.num_features:
+            feature = self.data['features'][self.i]
+            self.i += 1
+            return feature
+        else:
+            raise StopIteration
+
+    def next(self):
+        return self.next_feature()
+
+
+class GeoJSONLineParser(GeoJSONParser):
+    def __init__(self, filename):
+        self.f = open(filename)
+
+    def next_feature(self):
+        return json.loads(self.f.next().rstrip())
