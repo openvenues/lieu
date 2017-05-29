7ecc7bfb0a99c97a20ab060e48182426ffbdee6c
Al <albarrentine@gmail.com>
2017-05-29 05:57:07 -0400

    [input] mappings and constants for marshaling various input fields into libpostal's schema

---
+++ lib/lieu/address.py
@@ -0,0 +1,53 @@
+import six
+
+
+class AddressComponents:
+    NAME = 'house'
+    HOUSE_NUMBER = 'house_number'
+    STREET = 'road'
+    FLOOR = 'floor'
+    UNIT = 'unit'
+    POSTAL_CODE = 'postcode'
+
+
+class Coordinates:
+    LATITUDE = 'lat'
+    LONGITUDE = 'lon'
+
+
+class VenueDetails:
+    PHONE = 'phone'
+    WEBSITE = 'website'
+
+
+class Address(object):
+    field_map = {
+        'name': AddressComponents.NAME,
+        'addr:housename': AddressComponents.NAME,
+        'wof:name': AddressComponents.NAME,
+        'addr:housenumber': AddressComponents.HOUSE_NUMBER,
+        'addr:street': AddressComponents.STREET,
+        'addr:floor': AddressComponents.FLOOR,
+        'addr:level': AddressComponents.FLOOR,
+        'addr:unit': AddressComponents.UNIT,
+        'addr:postcode': AddressComponents.POSTAL_CODE,
+        'phone': VenueDetails.PHONE,
+        'sg:phone': VenueDetails.PHONE,
+        'contact:phone': VenueDetails.PHONE,
+        'telephone': VenueDetails.PHONE,
+        'sg:website': VenueDetails.WEBSITE,
+        'website': VenueDetails.WEBSITE,
+        'contact:website': VenueDetails.WEBSITE,
+    }
+
+    @classmethod
+    def from_geojson(cls, data):
+        properties = data.get('properties')
+        fields = {cls.field_map[k]: v for k, v in six.iteritems(properties) if k in cls.field_map}
+        lon, lat = data.get('geometry', {}).get('coordinates', (None, None))
+        if lat is not None:
+            fields[Coordinates.LATITUDE] = lat
+        if lon is not None:
+            fields[Coordinates.LONGITUDE] = lon
+
+        return fields
