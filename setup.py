844cff93e23c2526416e547b3743d5703f47271b
Al <albarrentine@gmail.com>
2017-10-08 19:38:32 -0400

    [setup] bumping version to 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 def main():
     setup(
         name='lieu',
-        version='0.1',
+        version='0.2',
         install_requires=[
             'six',
             'postal==1.0',
