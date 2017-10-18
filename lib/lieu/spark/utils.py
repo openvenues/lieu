71cba510595c64a5c89ec807dd01fdfba6b23282
Al <albarrentine@gmail.com>
2017-10-18 17:22:37 -0400

    [spark] coalesce partitions in IDPairRDD.join_pairs, should reduce the number of output files

--- lib/lieu/spark/utils.py
+++ lib/lieu/spark/utils.py
@@ -3,7 +3,11 @@
 class IDPairRDD(object):
     @classmethod
     def join_pairs(cls, pairs, kvs):
-        return pairs.join(kvs) \
-                    .map(lambda (k1, (k2, v1)): (k2, (k1, v1))) \
-                    .join(kvs) \
-                    .map(lambda (k2, ((k1, v1), v2)): ((k1, k2), (v1, v2)))
+        result = pairs.join(kvs) \
+                      .map(lambda (k1, (k2, v1)): (k2, (k1, v1)))
+
+        num_partitions = result.getNumPartitions()
+
+        return result.join(kvs) \
+                     .map(lambda (k2, ((k1, v1), v2)): ((k1, k2), (v1, v2))) \
+                     .coalesce(num_partitions)
