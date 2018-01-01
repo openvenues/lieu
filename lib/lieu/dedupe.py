f000aa2c24ee1fc0be84f6647e4ad1b5caa067de
Al <albarrentine@gmail.com>
2018-01-01 01:48:18 -0500

    [dedupe] using new libpostal/pypostal API in lieu dedupe module

--- lib/lieu/dedupe.py
+++ lib/lieu/dedupe.py
@@ -3,7 +3,10 @@ import geohash
 import re
 import six
 
-from postal.expand import expand_address, ADDRESS_NAME, ADDRESS_STREET, ADDRESS_UNIT, ADDRESS_LEVEL, ADDRESS_HOUSE_NUMBER, ADDRESS_POSTAL_CODE, ADDRESS_TOPONYM
+from postal.near_dupe import near_dupe_hashes
+from postal.dedupe import place_languages, duplicate_status, is_name_duplicate, is_street_duplicate, is_house_number_duplicate, is_po_box_duplicate, is_unit_duplicate, is_floor_duplicate, is_postal_code_duplicate, is_toponym_duplicate, is_name_duplicate_fuzzy, is_street_duplicate
+from postal.tokenize import tokenize
+from postal.token_types import token_types
 
 from lieu.address import AddressComponents, VenueDetails, Coordinates
 from lieu.api import DedupeResponse
@@ -11,37 +14,19 @@ from lieu.similarity import ordered_word_count, soft_tfidf_similarity, jaccard_s
 from lieu.encoding import safe_encode, safe_decode
 from lieu.floats import isclose
 
-double_metaphone = fuzzy.DMetaphone()
 whitespace_regex = re.compile('[\s]+')
 
 
 class AddressDeduper(object):
     DEFAULT_GEOHASH_PRECISION = 7
 
-    @classmethod
-    def component_equals(cls, c1, c2, component, no_whitespace=True):
-        if not c1 or not c2:
-            return False
-
-        c1 = safe_decode(c1)
-        c2 = safe_decode(c2)
-        if no_whitespace and whitespace_regex.sub(u'', c1.lower()) == whitespace_regex.sub(u'', c2.lower()):
-            return True
-
-        expansions1 = expand_address(c1, address_components=component)
-        expansions2 = expand_address(c2, address_components=component)
-
-        if not no_whitespace:
-            set_expansions1 = set(expansions1)
-            set_expansions2 = set(expansions2)
-        else:
-            set_expansions1 = set([whitespace_regex.sub(u'', e1) for e1 in expansions1])
-            set_expansions2 = set([whitespace_regex.sub(u'', e2) for e2 in expansions2])
-
-        return len(set_expansions1 & set_expansions2) > 0
+    address_only_keys = True
+    name_only_keys = False
+    name_and_address_keys = False
+    with_name = False
 
     @classmethod
-    def is_address_dupe(cls, a1, a2):
+    def is_address_dupe(cls, a1, a2, languages=None):
         a1_street = a1.get(AddressComponents.STREET)
         a2_street = a2.get(AddressComponents.STREET)
 
@@ -51,144 +36,152 @@ class AddressDeduper(object):
         if not a1_street or not a2_street or not a1_house_number or not a2_house_number:
             return None
 
-        same_street = cls.component_equals(a1_street, a2_street, ADDRESS_STREET)
-        same_house_number = cls.component_equals(a1_house_number, a2_house_number, ADDRESS_HOUSE_NUMBER)
+        same_street = is_street_duplicate(a1_street, a2_street, languages=languages)
+        same_house_number = is_house_number_duplicate(a1_house_number, a2_house_number, languages=languages)
 
         return same_street and same_house_number
 
     @classmethod
-    def is_sub_building_dupe(cls, a1, a2):
-        for key, component in ((AddressComponents.UNIT, ADDRESS_UNIT), (AddressComponents.FLOOR, ADDRESS_LEVEL)):
-            a1_field = a1.get(key)
-            a2_field = a2.get(key)
+    def is_sub_building_dupe(cls, a1, a2, languages=None):
+        a1_unit = a1.get(AddressComponents.UNIT)
+        a2_unit = a2.get(AddressComponents.UNIT)
 
-            if a1_field and a2_field:
-                if not cls.component_equals(a1_field, a2_field, component):
-                    return False
-            elif a1_field or a2_field:
-                return False
+        if a1_unit and a2_unit and not is_unit_duplicate(a1_unit, a2_unit, languages=languages):
+            return False
+        elif a1_unit or a2_unit:
+            return False
+
+        a2_floor = a1.get(AddressComponents.FLOOR)
+        a2_floor = a2.get(AddressComponents.FLOOR)
+
+        if a1_floor and a2_floor and not is_floor_duplicate(a1_floor, a2_floor, languages=languages):
+            return False
+        elif a1_floor or a2_floor:
+            return False
+
         return True
 
     @classmethod
+    def combined_languages(cls, languages1, languages2):
+        languages_set1 = set(languages1)
+
+        return languages1 + [lang for lang in languages2 if lang not in languages_set1]
+
+    @classmethod
+    def combined_place_languages(cls, a1, a2):
+        a1_keys, a1_values = cls.address_labels_and_values(a1)
+        a1_languages = place_languages(a1_keys, a1_values) or []
+
+        a2_keys, a2_values = cls.address_labels_and_values(a2)
+        a2_languages = place_languages(a2_keys, a2_values) or []
+
+        return cls.combined_languages(a1_languages, a2_languages)
+
+    @classmethod
     def is_dupe(cls, a1, a2, with_unit=True):
-        return cls.is_address_dupe(a1, a2) and (not with_unit or cls.is_sub_building_dupe(a1, a2))
+        languages = cls.combined_place_languages(a1, a2)
+
+        return cls.is_address_dupe(a1, a2, languages=languages) and (not with_unit or cls.is_sub_building_dupe(a1, a2, languages=languages))
 
     @classmethod
-    def component_expansions(cls, address):
-        street = address.get(AddressComponents.STREET)
-        house_number = address.get(AddressComponents.HOUSE_NUMBER)
+    def address_labels_and_values(cls, address):
+        string_address = {k: v for k, v in six.iteritems(address) if isinstance(v, six.string_types) and v.strip()}
 
-        if not (street and house_number):
-            return ()
-
-        street_expansions = expand_address(street, address_components=ADDRESS_STREET)
-        house_number_expansions = expand_address(house_number, address_components=ADDRESS_HOUSE_NUMBER)
-
-        return street_expansions, house_number_expansions
-
-    GEOHASH_KEY_PREFIX = 'gh'
-    POSTCODE_KEY_PREFIX = 'pc'
-    CITY_KEY_PREFIX = 'ct'
+        return string_address.keys(), string_address.values()
 
     @classmethod
-    def near_dupe_hashes(cls, address, geohash_precision=DEFAULT_GEOHASH_PRECISION, use_latlon=True, use_city=False, use_postal_code=False):
-        address_expansions = cls.component_expansions(address)
-
+    def near_dupe_hashes(cls, address, languages=None,
+                         with_address=True,
+                         with_unit=False,
+                         with_city_or_equivalent=False,
+                         with_small_containing_boundaries=False,
+                         with_postal_code=False,
+                         with_latlon=True,
+                         geohash_precision=DEFAULT_GEOHASH_PRECISION,
+                         name_and_address_keys=None,
+                         name_only_keys=None,
+                         address_only_keys=None):
         lat = address.get(Coordinates.LATITUDE)
         lon = address.get(Coordinates.LONGITUDE)
-        postcode = safe_decode(address.get(AddressComponents.POSTAL_CODE, u'')).strip()
-        city = safe_decode(address.get(AddressComponents.CITY, u'')).strip()
+        if lat is None or lon is None:
+            lat = 0.0
+            lon = 0.0
+            with_latlon = False
 
-        if not any(address_expansions):
-            return
+        labels, values = cls.address_labels_and_values(address)
 
-        if lat and lon and use_latlon and not ((isclose(lat, 0.0) and isclose(lon, 0.0)) or lat >= 90.0 or lat <= -90.0):
-            geo = geohash.encode(lat, lon)[:geohash_precision]
-            geohash_neighbors = [geo] + geohash.neighbors(geo)
+        if name_only_keys is None:
+            name_only_keys = cls.name_only_keys
 
-            base_key = cls.GEOHASH_KEY_PREFIX
+        if name_and_address_keys is None:
+            name_and_address_keys = cls.name_and_address_keys
 
-            for keys in six.itertools.product(geohash_neighbors, *address_expansions):
-                yield u'{}|{}'.format(base_key, u'|'.join(keys))
+        if address_only_keys is None:
+            address_only_keys = cls.address_only_keys
 
-        if postcode and use_postal_code:
-            postcode_expansions = expand_address(postcode, address_components=ADDRESS_POSTAL_CODE)
-
-            base_key = cls.POSTCODE_KEY_PREFIX
-
-            for keys in six.itertools.product(postcode_expansions, *address_expansions):
-                yield u'{}|{}'.format(base_key, u'|'.join(keys))
-
-        if city and use_city:
-            city_expansions = expand_address(city, address_components=ADDRESS_TOPONYM)
-
-            base_key = cls.CITY_KEY_PREFIX
-
-            for keys in six.itertools.product(city_expansions, *address_expansions):
-                yield u'{}|{}'.format(base_key, u'|'.join(keys))
+        return near_dupe_hashes(labels, values, languages=languages,
+                                with_name=cls.with_name,
+                                with_address=with_address,
+                                with_unit=with_unit,
+                                with_city_or_equivalent=with_city_or_equivalent,
+                                with_small_containing_boundaries=with_small_containing_boundaries,
+                                with_postal_code=with_postal_code,
+                                with_latlon=with_latlon,
+                                latitude=lat,
+                                longitude=lon,
+                                geohash_precision=geohash_precision,
+                                name_and_address_keys=name_and_address_keys,
+                                name_only_keys=name_only_keys,
+                                address_only_keys=cls.address_only_keys)
 
 
-class NameDeduper(object):
-    '''
-    Base class for deduping geographic entity names e.g. for matching names
-    from different databases (concordances).
-
-    By default uses Soft TFIDF similarity (see similarity.py)
-    for non-ideographic names and Jaccard similarity with word frequencies
-    for ideographic names.
-
-    See class attributes for options.
-    '''
-
-    ignore_parentheticals = True
-
+class Name(object):
     @classmethod
-    def tokenize(cls, s):
-        return s.split()
-
-    paren_regex = re.compile('\(.*\)')
-
-    @classmethod
-    def content_tokens(cls, s):
-        if cls.ignore_parentheticals:
-            tokens = cls.paren_regex.sub(u'', s)
-        return cls.tokenize(s.lower())
-
-    @classmethod
-    def compare_ideographs(cls, s1, s2):
-        tokens1 = cls.content_tokens(s1)
-        tokens2 = cls.content_tokens(s2)
-
-        if u''.join(tokens1) == u''.join(tokens2):
-            return 1.0
-        else:
-            # Many Han/Hangul characters are common, shouldn't use IDF
-            return jaccard_similarity(tokens1, tokens2)
-
-    @classmethod
-    def compare_in_memory(cls, tokens1, tokens2, tfidf):
-        # Test exact equality, also handles things like Cabbage Town == Cabbagetown
-        token_counts1 = ordered_word_count(tokens1)
-        token_counts2 = ordered_word_count(tokens2)
-
-        tfidf1 = tfidf.tfidf_vector(token_counts1)
-        tfidf2 = tfidf.tfidf_vector(token_counts2)
-
-        tfidf1_norm = tfidf.normalized_tfidf_vector(tfidf1)
-        tfidf2_norm = tfidf.normalized_tfidf_vector(tfidf2)
-
-        return soft_tfidf_similarity(tfidf1_norm, tfidf2_norm)
+    def content_tokens(cls, name):
+        return [t for t, c in tokenize(name) if c in token_types.WORD_TOKEN_TYPES or c in token_types.NUMERIC_TOKEN_TYPES]
 
 
 class VenueDeduper(AddressDeduper):
     DEFAULT_GEOHASH_PRECISION = 6
 
-    @classmethod
-    def name_similarity(cls, a1_name, a2_name, tfidf):
-        a1_name_tokens = NameDeduper.content_tokens(a1_name)
-        a2_name_tokens = NameDeduper.content_tokens(a2_name)
+    address_only_keys = False
+    name_only_keys = False
+    name_and_address_keys = True
+    with_name = True
 
-        return NameDeduper.compare_in_memory(a1_name_tokens, a2_name_tokens, tfidf)
+    @classmethod
+    def tfidf_vector(cls, tokens, tfidf_index):
+        token_counts = ordered_word_count(tokens1)
+        return tfidf_index.tfidf_vector(token_counts)
+
+    @classmethod
+    def tfidf_vector_normalized(cls, tokens, tfidf_index):
+        tfidf = cls.tfidf_vector(tokens, tfidf_index)
+        return tfidf.normalized_tfidf_vector(tfidf)
+
+    dupe_class_map = {
+        duplicate_status.LIKELY_DUPLICATE: DedupeResponse.classifications.LIKELY_DUPE,
+        duplicate_status.EXACT_DUPLICATE: DedupeResponse.classifications.EXACT_DUPE,
+        duplicate_status.NEEDS_REVIEW: DedupeResponse.classifications.NEEDS_REVIEW,
+    }
+
+    @classmethod
+    def string_dupe_class(cls, dupe_class):
+        return cls.dupe_class_map.get(dupe_class)
+
+    @classmethod
+    def name_dupe_similarity(cls, a1_name, a2_name, tfidf, languages=None, likely_dupe_threshold=DedupeResponse.default_name_dupe_threshold,
+                             needs_review_threshold=DedupeResponse.default_name_review_threshold):
+        a1_name_tokens = Name.content_tokens(a1_name)
+        a2_name_tokens = Name.content_tokens(a2_name)
+        if not a1_name_tokens or not a2_name_tokens:
+            return None, 0.0
+
+        a1_tfidf_norm = cls.tfidf_vector_normalized(a1_name_tokens, tfidf)
+        a2_tfidf_norm = cls.tfidf_vector_normalized(a2_name_tokens, tfidf)
+
+        return is_name_duplicate_fuzzy(a1_name_tokens, a1_tfidf_norm, a2_name_tokens, a2_tfidf_norm, languages=languages,
+                                       likely_dupe_threshold=likely_dupe_threshold, needs_review_threshold=needs_review_threshold)
 
     @classmethod
     def dupe_class_and_sim(cls, a1, a2, tfidf=None, name_dupe_threshold=DedupeResponse.default_name_dupe_threshold,
@@ -198,24 +191,23 @@ class VenueDeduper(AddressDeduper):
         if not a1_name or not a2_name:
             return None, 0.0
 
-        same_address = cls.is_address_dupe(a1, a2)
+        languages = cls.combined_place_languages(a1, a2)
+
+        same_address = cls.is_address_dupe(a1, a2, languages=languages)
         if not same_address:
             return None, 0.0
 
         if with_unit:
-            same_unit = cls.is_sub_building_dupe(a1, a2)
+            same_unit = cls.is_sub_building_dupe(a1, a2, languages=languages)
             if not same_unit:
                 return None, 0.0
 
-        exact_same_name = cls.is_exact_name_dupe(a1_name, a2_name)
+        exact_same_name = cls.is_exact_name_dupe(a1_name, a2_name, languages=languages)
         if exact_same_name:
             return DedupeResponse.classifications.EXACT_DUPE, 1.0
         elif tfidf:
-            name_sim = cls.name_similarity(a1_name, a2_name, tfidf)
-            if name_sim >= name_dupe_threshold:
-                return DedupeResponse.classifications.LIKELY_DUPE, name_sim
-            elif name_sim >= name_review_threshold:
-                return DedupeResponse.classifications.NEEDS_REVIEW, name_sim
+            dupe_class, name_sim = cls.name_dupe_similarity(a1_name, a2_name, tfidf)
+            return cls.string_dupe_class(dupe_class), name_sim
 
         return None, 0.0
 
@@ -225,35 +217,5 @@ class VenueDeduper(AddressDeduper):
         return dupe_class in (DedupeResponse.classifications.EXACT_DUPE, DedupeResponse.classifications.LIKELY_DUPE)
 
     @classmethod
-    def is_exact_name_dupe(cls, name1, name2):
-        return cls.component_equals(name1, name2, ADDRESS_NAME)
-
-    @classmethod
-    def name_word_hashes(cls, name):
-        name_expanded_words = set()
-
-        for n in expand_address(name, address_components=ADDRESS_NAME):
-            tokens = NameDeduper.tokenize(n)
-            for t in tokens:
-                dm = set([e for e in double_metaphone(safe_encode(t)) if e is not None])
-                if dm:
-                    name_expanded_words |= dm
-                else:
-                    name_expanded_words.add(t)
-
-        return name_expanded_words
-
-    @classmethod
-    def component_expansions(cls, address):
-        name = address.get(AddressComponents.NAME)
-
-        if not name:
-            return ()
-
-        expansions = super(cls, VenueDeduper).component_expansions(address)
-        if not expansions:
-            return ()
-
-        name_expanded_words = cls.name_word_hashes(name)
-
-        return (list(name_expanded_words),) + expansions
+    def is_exact_name_dupe(cls, name1, name2, languages=None):
+        return is_name_duplicate(name1, name2, languages=languages)
