1d2fc5329f9b1679e6c06b4a3156510c98f08d28
Al <albarrentine@gmail.com>
2019-08-11 15:17:41 -0400

    [spark] adding join_pairs_multi_kv for where the pairs join to two different RDDs

--- lib/lieu/spark/utils.py
+++ lib/lieu/spark/utils.py
@@ -11,3 +11,14 @@ class IDPairRDD(object):
         return result.join(kvs) \
                      .map(lambda (k2, ((k1, v1), v2)): ((k1, k2), (v1, v2))) \
                      .coalesce(num_partitions)
+
+    @classmethod
+    def join_pairs_multi_kv(cls, pairs, kvs1, kvs2):
+        result = pairs.join(kvs1) \
+                      .map(lambda (k1, (k2, v1)): (k2, (k1, v1)))
+
+        num_partitions = result.getNumPartitions()
+
+        return result.join(kvs2) \
+                     .map(lambda (k2, ((k1, v1), v2)): ((k1, k2), (v1, v2))) \
+                     .coalesce(num_partitions)
